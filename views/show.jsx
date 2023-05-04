d3603e0813ea55ae52e5f7058412e4a3c22ea38e
Holden <120606057+HCN1202@users.noreply.github.com>
2023-05-04 19:57:07 -0400

    update bread

--- views/show.jsx
+++ views/show.jsx
@@ -2,7 +2,6 @@ const React = require('react')
 const Default = require('./layouts/default')
 
 function Show( {bread, index} ) {
-    console.log(bread)
     return (
         <Default> 
             <h3>{bread.name}</h3>
@@ -16,6 +15,11 @@ function Show( {bread, index} ) {
             <img src={bread.image} alt={bread.name}/>
             <li>
                 <a href={'/breads'}>Go home</a>
+            </li>
+            <li>
+                <a href={`/breads/${index}/edit`}>
+                    <button>Edit</button>
+                </a>
             </li>
             <form action={`/breads/${index}?_method=DELETE`} method='POST'>
                 <input type='submit' value='DELETE' />
